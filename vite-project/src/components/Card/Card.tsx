d6190cea21f6473bc1cc3aa893ccb37402db9e58
Mohammad Mahdi Mottaghi <mottaghimohammadmahdi@gmail.com>
2023-04-21 15:42:40 +0330

    edit project

--- vite-project/src/components/Card/Card.tsx
+++ vite-project/src/components/Card/Card.tsx
@@ -1,28 +1,43 @@
-import { Counter } from '../Counter/Counter';
-import { useState } from 'react';
+import { Counter } from "../Counter/Counter";
+import { useState } from "react";
 
 type card = {
   title: string;
   price: number;
   src: string;
+  totalAmount: number;
+  setTotalAmount: React.Dispatch<React.SetStateAction<number>>;
 };
 
-export const Card = (props: card) => {
+export const Card = ({
+  title,
+  src,
+  price,
+  totalAmount,
+  setTotalAmount,
+}: card) => {
   const [num, setNum] = useState(0);
+
   return (
     <div className="w-full flex justify-between bg-white p-3 rounded-md col-span-1">
       <div className="w-full flex gap-3">
         <div>
-          <img src={props.src} className="w-20 h-full" alt="" />
+          <img src={src} className="w-20 h-full" alt="" />
         </div>
         <div className="w-full flex flex-col gap-4">
           <div>
-            <div className="text-lg font-semibold">{props.title}</div>
-            <div className="text-lg">تومان {props.price}</div>
+            <div className="text-lg font-semibold">{title}</div>
+            <div className="text-lg">تومان {price}</div>
           </div>
           <div className="w-full flex">
-            <Counter num={num} setNum={setNum} />
-            <div className="mr-auto">تومان 0</div>
+            <Counter
+              singleProductPrice={price}
+              num={num}
+              setNum={setNum}
+              totalAmount={totalAmount}
+              setTotalAmount={setTotalAmount}
+            />
+            <div className="mr-auto">تومان {price * num}</div>
           </div>
         </div>
       </div>
--- vite-project/src/components/Counter/Counter.tsx
+++ vite-project/src/components/Counter/Counter.tsx
@@ -1,33 +1,44 @@
-import { Button } from '../Button/Button';
+import { Button } from "../Button/Button";
 
 interface Iprops {
   setNum: React.Dispatch<React.SetStateAction<number>>;
+  singleProductPrice: number;
   num: number;
+  totalAmount: number;
+  setTotalAmount: React.Dispatch<React.SetStateAction<number>>;
 }
 
-export const Counter = ({ setNum, num }: Iprops) => {
+export const Counter = ({
+  setNum,
+  num,
+  singleProductPrice,
+  totalAmount,
+  setTotalAmount,
+}: Iprops) => {
   return (
     <div className="flex gap-2">
       <Button
         onClick={() => {
-          return setNum(num + 1);
+          setNum(num + 1);
+          setTotalAmount(totalAmount + singleProductPrice);
         }}
         style={
-          'rounded-r-md text-white bg-red-500 w-6 flex items-center justify-center'
+          "rounded-r-md text-white bg-red-500 w-6 flex items-center justify-center"
         }
-        children={'+'}
+        children={"+"}
       />
       <span className="px-1">{num}</span>
       <Button
         onClick={() => {
           if (num > 0) {
-            return setNum(num - 1);
+            setNum(num - 1);
+            setTotalAmount(totalAmount - singleProductPrice);
           }
         }}
         style={
-          'rounded-l-md text-white bg-red-500 w-6 flex items-center justify-center'
+          "rounded-l-md text-white bg-red-500 w-6 flex items-center justify-center"
         }
-        children={'-'}
+        children={"-"}
       />
     </div>
   );
